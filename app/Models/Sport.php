a0b040fc61ae87f35bf296202be5cc39e30c0e69
dariinaa <darinavasileva121@gmail.com>
2024-01-10 21:04:59 +0200

    fixed README

--- app/Models/Sport.php
+++ app/Models/Sport.php
@@ -42,20 +42,15 @@ class Sport extends Model
 	public function setImageAttribute($value)
 	{
 		$attribute_name = "image";
-		// destination path relative to the disk above
 		$destination_path = "sports";
-		// if the image was erased
 		if ($value==null) 
 		{
-			// delete the image from disk
 			Storage::delete(Str::replaceFirst('storage/','public/',$this->{$attribute_name}));
-			// set null in the database column
 			$this->attributes[$attribute_name] = null;
 		}
 		else
 		{
 			$disk = "public";
-			// filename is generated -  md5($file->getClientOriginalName().random_int(1, 9999).time()).'.'.$file->getClientOriginalExtension()
 			$this->uploadFileToDisk($value, $attribute_name, $disk, $destination_path, $fileName = null);
 			$this->attributes[$attribute_name] = 'storage/' . $this->attributes[$attribute_name];
 		}
